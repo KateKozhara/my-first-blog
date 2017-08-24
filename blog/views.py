b628d16ff7701ded53638499367886d73547b98d
KateKozhara <kozara2903@gmail.com>
2017-08-24 18:43:57 +0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from . models import Post
-
+from .models import Post
 
 def KateK(request):
-	return render(request, 'blog/KateK.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/Katek.html', {'posts': posts})
 
