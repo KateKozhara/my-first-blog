de5ce611675995bf7f09040a95cafe0f17a2bcdc
KateKozhara <kozara2903@gmail.com>
2017-08-24 18:02:25 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from . models import Post
 
 
 def KateK(request):
-    return render(request, 'blog/KateK.html', {})
-# Create your views here.
+	return render(request, 'blog/KateK.html', {})
+
